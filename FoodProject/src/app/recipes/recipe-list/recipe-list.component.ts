45d0e3285494feb8269ddeea8d398fd6a539d084
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-06-06 14:36:13 +0200

    Setting up a list of recipes

--- FoodProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ FoodProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Salmon', 
+      'The best fish', 
+      'https://diethood.com/wp-content/uploads/2018/10/Garlic-Butter-Baked-Salmon-12.jpg'),
+    new Recipe(
+      'Pasta', 
+      'Good food', 
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-bolognese_2.jpg')
+    ];
 
   constructor() { }
 
