d83c606dca3553a9f55580c5a61083733b580573
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-06-17 11:32:13 +0200

    Another way of changing background color with HostBinding

--- DirectivesProject/src/app/better-highlight/better-highlight.directive.ts
+++ DirectivesProject/src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'; // in () has to be camelCasing
+
   constructor( private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
@@ -11,10 +13,12 @@ export class BetterHighlightDirective implements OnInit{
   }
 
   @HostListener('mouseover') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
