1155c12830213d9869801d949a7a0042bfde005e
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-06-17 10:15:53 +0200

    Our custom attribute directive

---
+++ DirectivesProject/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+    selector: '[appBasicHighlight]' // [] tell Angular to select it as an attribute
+})
+export class BasicHighlightDirective implements OnInit {
+    constructor(private elementRef: ElementRef) {
+    }
+
+    ngOnInit() {
+        this.elementRef.nativeElement.style.backgroundColor = 'green';
+    }
+}
