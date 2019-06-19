d21bdd4eefa5c8efbc7bd0d22842406ef541c763
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-06-19 11:19:33 +0200

    Adding ingredients and passing them from recipes to the shopping list + small other tweaks

--- FoodProject/src/app/recipes/recipe.service.ts
+++ FoodProject/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   
@@ -8,14 +11,29 @@ export class RecipeService {
     new Recipe(
       'Salmon', 
       'The best fish', 
-      'https://diethood.com/wp-content/uploads/2018/10/Garlic-Butter-Baked-Salmon-12.jpg'),
+      'https://diethood.com/wp-content/uploads/2018/10/Garlic-Butter-Baked-Salmon-12.jpg',
+      [
+        new Ingredient('Salmon', 1),
+        new Ingredient('Rice', 1)
+      ]),
     new Recipe(
       'Pasta', 
       'Good food', 
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-bolognese_2.jpg')
-    ];
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-bolognese_2.jpg',
+      [
+        new Ingredient('Macaroni', 1),
+        new Ingredient('Meatballs', 2),
+        new Ingredient('Tomato sauce', 1)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice(); // returns new array, an exact copy
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
