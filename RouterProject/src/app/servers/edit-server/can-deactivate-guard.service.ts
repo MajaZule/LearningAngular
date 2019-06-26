e13d6fb54e1ca8a07d5462a4a3e220d3d3ea798b
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-06-26 11:20:26 +0200

    Small change

--- RouterProject/src/app/servers/edit-server/can-deactivate-guard.service.ts
+++ RouterProject/src/app/servers/edit-server/can-deactivate-guard.service.ts
@@ -10,7 +10,7 @@ export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate>
     canDeactivate(component: CanComponentDeactivate,
                   currentRoute: ActivatedRouteSnapshot,
                   currentState: RouterStateSnapshot,
-                  nextState: RouterStateSnapshot
+                  nextState?: RouterStateSnapshot
     ): Observable<boolean> | Promise<boolean> | boolean {
         return component.CanDeactivate();
     }
