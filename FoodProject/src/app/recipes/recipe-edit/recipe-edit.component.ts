0eb6b0eac6be005824176bff3fc92e93cfa1cae9
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-06-26 15:09:40 +0200

    Retrieving route params - checking in which mode we are

--- FoodProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ FoodProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = params['id'] != null; // we are checking if id exists
+      }
+    );
   }
-
+// when the params change: if params has an ['id'] and checking, if it is not null,
+// we are checking if it has an id. It will not be indefined if we are in edit mode;
+// id will be present. if true and id is undefined ( == null ), we are in new mode.
 }
