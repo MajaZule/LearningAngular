a412f3f13517e48de648b90e6c6f73b884a5bf81
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-06-27 15:20:21 +0200

    Submit the form and extract the data, then output it on the website

--- Forms/src/app/app.component.ts
+++ Forms/src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   answer = '';
   genders = ['male', 'female'];
   defaultGender = 'male';
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -36,6 +44,11 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
