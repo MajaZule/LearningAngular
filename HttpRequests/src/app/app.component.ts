1ee317025f297e2ab63069000ab8c883d17efce6
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-02 14:54:48 +0200

    Sending an http request (post) - code already in the initial commit!

--- HttpRequests/src/app/app.component.ts
+++ HttpRequests/src/app/app.component.ts
@@ -15,14 +15,12 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+    this.http.post(
+      'https://my-http-project-f9c5f.firebaseio.com/posts.json',
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
