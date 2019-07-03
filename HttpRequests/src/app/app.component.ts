c002fb3605b4c74d226bd66fe6f55420014281b0
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-03 12:36:56 +0200

    Adding button to get rid of the error message

--- HttpRequests/src/app/app.component.ts
+++ HttpRequests/src/app/app.component.ts
@@ -27,6 +27,7 @@ export class AppComponent implements OnInit, OnDestroy {
       this.isFetching = false;
       this.loadedPosts = posts;
     }, error => {
+      this.isFetching = false;
       this.error = error.message;
     });
   }
@@ -43,6 +44,7 @@ export class AppComponent implements OnInit, OnDestroy {
       this.isFetching = false;
       this.loadedPosts = posts;
     }, error => {
+      this.isFetching = false;
       this.error = error.message;
       console.log(error);
     });
@@ -53,6 +55,10 @@ export class AppComponent implements OnInit, OnDestroy {
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  onHandleError() {
+    this.error = null;
   }
 
   ngOnDestroy() {
