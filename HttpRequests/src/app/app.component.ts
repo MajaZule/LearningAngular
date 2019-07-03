2214dd4bdb3f794d880b1f8f56f5d045dbcadb35
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-03 10:33:49 +0200

    Loading indicator

--- HttpRequests/src/app/app.component.ts
+++ HttpRequests/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>('https://my-http-project-f9c5f.firebaseio.com/posts.json')
       .pipe(
@@ -51,7 +53,8 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
-      this.loadedPosts = posts;
+        this.isFetching = false;
+        this.loadedPosts = posts;
     });
   }
 }
