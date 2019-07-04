de9330756d5db5995199dce8b491962f2dcf92e6
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-04 09:46:48 +0200

    Fetching recipes from a database

--- FoodProject/src/app/shared/data-storage.service.ts
+++ FoodProject/src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({providedIn: 'root'})      // we have to use it because we are injecting a service into a service
 export class DataStorageService {
@@ -17,5 +18,13 @@ export class DataStorageService {
     .subscribe(response => {
       console.log(response);
     });  
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>('https://course-recipe-book-aebb9.firebaseio.com/recipes.json')
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
