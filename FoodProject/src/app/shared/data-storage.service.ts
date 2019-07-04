d005fbfdc8ea20012fbab0d5885142538778897e
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-04 09:27:16 +0200

    Storing recipes into a database

--- FoodProject/src/app/shared/data-storage.service.ts
+++ FoodProject/src/app/shared/data-storage.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})      // we have to use it because we are injecting a service into a service
 export class DataStorageService {
   
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http
+    .put(                  // put is a firebase method
+      'https://course-recipe-book-aebb9.firebaseio.com/recipes.json', 
+      recipes
+    )
+    .subscribe(response => {
+      console.log(response);
+    });  
+  }
 }
