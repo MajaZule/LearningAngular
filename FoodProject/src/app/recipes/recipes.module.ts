87409a5d481bcaa3226cb2edda9dd98f67631759
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-05 15:30:10 +0200

    Creating recipes module

---
+++ FoodProject/src/app/recipes/recipes.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeEditComponent,
+    RecipeStartComponent,
+    RecipeItemComponent
+  ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeEditComponent,
+    RecipeStartComponent,
+    RecipeItemComponent
+  ]
+})
+export class RecipesModule {}
