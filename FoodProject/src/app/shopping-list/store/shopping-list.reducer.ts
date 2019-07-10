5d3dbdcbab42b80eb286807477cacdf6693e7f8b
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-10 14:42:36 +0200

    Updating and deleting ingredients

--- FoodProject/src/app/shopping-list/store/shopping-list.reducer.ts
+++ FoodProject/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -9,17 +9,45 @@ export function shoppingListReducer(
   state = initialState,
   action: ShoppingListActions.ShoppingListActions
 ) {
+
   switch (action.type) {
+    
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action.payload]
       };
+    
     case ShoppingListActions.ADD_INGREDIENTS:
       return {
         ...state,
         ingredients: [...state.ingredients, ...action.payload]
       };
+    
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[action.payload.index] = updatedIngredient;
+      // we are replacing the old element with index in the copied updatedIngredients[] 
+      // with the copied updatedIngredient
+
+      return {
+        ...state,
+        ingredients: updatedIngredients
+      };
+    
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((ig, igIndex) => { // filter returns a new copy
+          return igIndex !== action.payload;
+        }) 
+      };
+    
     default:
       return state;
   }
