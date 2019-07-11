727fb53d13548c29a93084410fb17bb9aaa47079
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-11 11:25:30 +0200

    Replacing user model with store

--- FoodProject/src/app/auth/auth.guard.ts
+++ FoodProject/src/app/auth/auth.guard.ts
@@ -3,17 +3,27 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { map, take } from 'rxjs/operators';
+import * as fromApp from '../store/app.reducer';
+import { Store } from '@ngrx/store';
+
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private store: Store<fromApp.AppState>
+  ) {}
   
   canActivate(
     route: ActivatedRouteSnapshot, 
     router: RouterStateSnapshot
   ): boolean | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> | UrlTree {
-    return this.authService.user.pipe(
+    return this.store.select('auth').pipe(
       take(1),
+      map(authState => {
+        return authState.user;
+      }),
       map(user => { // we just return this because it is already an observable
       const isAuth = !!user;
       if (isAuth) {
