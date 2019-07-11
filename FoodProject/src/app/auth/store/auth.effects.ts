b14801343ad7f38016a612c43b203804e423aa42
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-11 15:36:00 +0200

    Preparing other auth actions

--- FoodProject/src/app/auth/store/auth.effects.ts
+++ FoodProject/src/app/auth/store/auth.effects.ts
@@ -19,6 +19,10 @@ export interface AuthResponseData {
 }
 @Injectable()
 export class AuthEffects {
+  authSignup = this.actions$.pipe(
+    ofType(AuthActions.SIGNUP_START)
+  );
+
   @Effect()
   authLogin = this.actions$.pipe(
     ofType(AuthActions.LOGIN_START), // only login_start will trigger this
@@ -36,7 +40,7 @@ export class AuthEffects {
           const expiratoionDate = new Date(
             new Date().getTime() + +resData.expiresIn * 1000
           );    
-          return new AuthActions.Login({
+          return new AuthActions.AuthenticateSuccess({
             email: resData.email, 
             userId: resData.localId, 
             token: resData.idToken, 
@@ -46,7 +50,7 @@ export class AuthEffects {
         catchError(errorRes => { // must return non-error observable
           let errorMessage = 'An unknown error occured!';
           if (!errorRes.error || !errorRes.error.error) {
-            return of(new AuthActions.LoginFail(errorMessage));
+            return of(new AuthActions.AuthenticateFail(errorMessage));
           }
           switch (errorRes.error.error.message) {
             case 'EMAIL_EXISTS':
@@ -59,7 +63,7 @@ export class AuthEffects {
               errorMessage = 'Password is incorect!';
               break;
           }
-          return of(new AuthActions.LoginFail(errorMessage));
+          return of(new AuthActions.AuthenticateFail(errorMessage));
         })
       );
     })
@@ -67,7 +71,7 @@ export class AuthEffects {
 
   @Effect({ dispatch: false })
   authSuccess = this.actions$.pipe(
-    ofType(AuthActions.LOGIN), 
+    ofType(AuthActions.AUTHENTICATE_SUCCESS), 
     tap(() => {
       this.router.navigate(['/']);
     })
