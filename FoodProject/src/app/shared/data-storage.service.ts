fa815877bace77c9cf45e69cf3e659ea1b9cea62
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-12 13:19:46 +0200

    Setting up NgRx for recipes

--- FoodProject/src/app/shared/data-storage.service.ts
+++ FoodProject/src/app/shared/data-storage.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map, tap, take, exhaustMap } from 'rxjs/operators';
-import { AuthService } from '../auth/auth.service';
+import { map, tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
 
 @Injectable({providedIn: 'root'})      // we have to use it because we are injecting a service into a service
 export class DataStorageService {
@@ -11,7 +13,7 @@ export class DataStorageService {
   constructor(
     private http: HttpClient, 
     private recipeService: RecipeService,
-    private authService: AuthService
+    private store: Store<fromApp.AppState>
   ) {}
 
   storeRecipes() {
@@ -40,7 +42,8 @@ export class DataStorageService {
         }); 
       }),
       tap(recipes => {
-        this.recipeService.setRecipes(recipes);
+        // this.recipeService.setRecipes(recipes);
+        this.store.dispatch(new RecipesActions.SetRecipes(recipes));
       })
     );
   }
