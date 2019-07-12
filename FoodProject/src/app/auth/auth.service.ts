7ef9c7bd43d86c70af13353049a8b0618f0677df
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-12 10:28:39 +0200

    Adding auto login with NgRx

--- FoodProject/src/app/auth/auth.service.ts
+++ FoodProject/src/app/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError, tap } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
--- FoodProject/src/app/auth/store/auth.effects.ts
+++ FoodProject/src/app/auth/store/auth.effects.ts
@@ -7,6 +7,7 @@ import { of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { dispatch } from 'rxjs/internal/observable/range';
+import { User } from '../user.model';
 
 export interface AuthResponseData {
   kind: string,
@@ -26,7 +27,9 @@ const handleAuthentication = (
 ) => {
   const expiratoionDate = new Date(
     new Date().getTime() + expiresIn * 1000
-  );    
+  );  
+  const user = new User(email, userId, token, expiratoionDate);  
+  localStorage.setItem('userData', JSON.stringify(user));
   return new AuthActions.AuthenticateSuccess({
     email: email, 
     userId: userId, 
@@ -119,6 +122,53 @@ export class AuthEffects {
   );
   // this effect does not dispatch a new action
   // we have to add object to effects decorator
+
+  @Effect()
+  autoLogin = this.actions$.pipe(
+    ofType(AuthActions.AUTO_LOGIN),
+    map(() => {
+      const userData: {
+        email: string;
+        id: string;
+        _token: string;
+        _tokenExpirationDate: string;
+      } = JSON.parse(localStorage.getItem('userData'));
+      
+      if (!userData) {
+        return { type: 'DUMMY' }; 
+      }
+  
+      const loadedUser = new User(
+        userData.email, 
+        userData.id, 
+        userData._token, 
+        new Date(userData._tokenExpirationDate)
+      );
+  
+      if (loadedUser.token) {  
+        // this.user.next(loadedUser);
+        return new AuthActions.AuthenticateSuccess({
+          email: loadedUser.email, 
+          userId: loadedUser.id, 
+          token: loadedUser.token, 
+          expirationDate: new Date(userData._tokenExpirationDate)
+        });
+        // const expirationDuration = 
+        //   new Date(userData._tokenExpirationDate).getTime() - 
+        //   new Date().getTime();
+        // this.autoLogout(expirationDuration);
+      }
+      return { type: 'DUMMY' };
+    })
+  )
+
+  @Effect({ dispatch: false })
+  authLogout = this.actions$.pipe(
+    ofType(AuthActions.LOGOUT), 
+    tap(() => {
+      localStorage.removeItem('userData');
+    })
+  )
 
   constructor(
     private actions$: Actions, 
