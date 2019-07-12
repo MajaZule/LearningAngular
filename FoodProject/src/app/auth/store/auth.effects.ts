ef3473bc9ef6ace2124d35d8484fffe65c97ee30
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-07-12 14:50:20 +0200

    Fixing the auth redirect and previous bug

--- FoodProject/src/app/auth/store/auth.effects.ts
+++ FoodProject/src/app/auth/store/auth.effects.ts
@@ -34,7 +34,8 @@ const handleAuthentication = (
     email: email, 
     userId: userId, 
     token: token, 
-    expirationDate: expiratoionDate
+    expirationDate: expiratoionDate,
+    redirect: true
   });
 }
 
@@ -122,8 +123,10 @@ export class AuthEffects {
   @Effect({ dispatch: false })
   authRedirect = this.actions$.pipe(
     ofType(AuthActions.AUTHENTICATE_SUCCESS), 
-    tap(() => {
-      this.router.navigate(['/']);
+    tap((authSuccessAction: AuthActions.AuthenticateSuccess) => {
+      if (authSuccessAction.payload.redirect) {
+        this.router.navigate(['/']);
+      }
     })
   );
   // this effect does not dispatch a new action
@@ -161,7 +164,8 @@ export class AuthEffects {
           email: loadedUser.email, 
           userId: loadedUser.id, 
           token: loadedUser.token, 
-          expirationDate: new Date(userData._tokenExpirationDate)
+          expirationDate: new Date(userData._tokenExpirationDate),
+          redirect: false
         });
         // const expirationDuration = 
         //   new Date(userData._tokenExpirationDate).getTime() - 
